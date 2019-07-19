d7e9afd54b39e8ca2e6c9a4420aaecbb8f1bee8a
Reed James <reedtjames1234@gmail.com>
2019-07-19 10:25:16 -0500

    WIP, componets framed, essay questions complete

---
+++ client/client/src/components/UserList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import User from "./User";
+
+const UserList = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <User />
+    </div>
+  );
+};
+
+export default UserList;
