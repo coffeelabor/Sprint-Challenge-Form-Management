991370115875b3ed30ac631cb35d7d9107d7c357
Reed James <reedtjames1234@gmail.com>
2019-07-19 11:08:18 -0500

    WIP added From Feilds, added token functiality, local storage token functional

--- client/client/src/components/Login.js
+++ client/client/src/components/Login.js
@@ -1,11 +1,43 @@
 import React from "react";
+import { withFormik, Form, Field } from "formik";
+import axios from "axios";
 
-const Login = () => {
+const Login = ({ values, errors, touched, isSubmitting }) => {
   return (
-    <div>
+    <Form>
       <h1>Howdy</h1>
-    </div>
+      <div>
+        <Field type="text" name="username" placeholder="Username" />
+        <p>{touched.username && errors.username}</p>
+      </div>
+      <div>
+        <Field type="password" name="password" placeholder="Password" />
+        <p>{touched.password && errors.password}</p>
+      </div>
+      <button disabled={isSubmitting}>Submit</button>
+    </Form>
   );
 };
 
-export default Login;
+export default withFormik({
+  mapPropsToValues: () => {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  handleSubmit: (values, formikBag) => {
+    console.log("Values inside handlesubmit", values);
+    const url = "http://localhost:5000/api/register";
+    axios
+      .post(url, values)
+      .then(res => {
+        console.log("Res inside .then", res);
+        localStorage.setItem("token", res.data.token);
+        formikBag.props.history.push("/user");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+})(Login);
