1d894b9cf548817c8beeb88f42e464588a950453
Reed James <reedtjames1234@gmail.com>
2019-07-19 10:43:27 -0500

    WIP, indes.js added broweser router, App.js add routes and links, functional

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Link, Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import UserList from "./components/UserList";
 import "./App.css";
@@ -7,8 +8,24 @@ function App() {
   return (
     <div className="App">
       <h1>hello</h1>
-      <Login />
-      <UserList />
+      <div>
+        <Link to="/">Login</Link>
+        <Link to="/user">Users</Link>
+      </div>
+      <Route exact path="/" component={Login} />
+      <Route
+        exact
+        path="/user"
+        render={props => {
+          const token = localStorage.getItem("token");
+          if (!token) {
+            return <Redirect to="/" />;
+          }
+          return <UserList {...props} />;
+        }}
+      />
+      {/* <Login />
+      <UserList /> */}
     </div>
   );
 }
